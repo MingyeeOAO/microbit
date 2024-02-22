00581fc730ffbb8997012b74f0b2f9fd473e5b32
Mingyee <87744503+MingyeeOAO@users.noreply.github.com>
2024-02-22 14:29:56 +0800

    Update Vector.ts

--- lib/Vector.ts
+++ lib/Vector.ts
@@ -1,1 +1,20 @@
 
+class Vector2 {
+    x: number
+    y: number
+    length : number
+    constructor(x: number = 0, y: number = 0) {
+        this.x = x
+        this.y = y
+        this.update()
+    }
+
+    private update(){
+        this.length = this.__len__()
+    }
+
+    private __len__(): number {
+        return Math.sqrt(this.x ** 2 + this.y ** 2)
+    }
+    
+}
