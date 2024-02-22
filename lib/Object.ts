40eae58f45a46ea87b58bedf17037af9544e1673
Mingyee <87744503+MingyeeOAO@users.noreply.github.com>
2024-02-22 14:49:39 +0800

    Update Object.ts

--- lib/Object.ts
+++ lib/Object.ts
@@ -1,6 +1,7 @@
 class Object{
     position : Vector2
     velocity : Vector2
-    constructer(position: Vector2, velocity: Vector2){
+    constructer(position: Vector2){
          this.position = position
-        
+    }
+}
