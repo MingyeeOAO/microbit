689e9d7187a3d559d4455bddd0a1defebf818a0d
Mingyee <87744503+MingyeeOAO@users.noreply.github.com>
2024-02-22 14:25:46 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,24 @@
-//  v = Vector2(1, 2)
-basic.forever(function on_forever() {
-    basic.showNumber(200)
+class Vector2 {
+    x: number
+    y: number
+    length : number
+    constructor(x: number = 0, y: number = 0) {
+        this.x = x
+        this.y = y
+        this.update()
+    }
+
+    private update(){
+        this.length = this.__len__()
+    }
+
+    private __len__(): number {
+        return Math.sqrt(this.x ** 2 + this.y ** 2)
+    }
+    
+}
+let v = new Vector2(4, 3)
+basic.showNumber(v.length)
+basic.forever(function () {
+    basic.showNumber(1)
 })
