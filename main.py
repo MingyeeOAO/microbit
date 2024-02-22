2bdc83ecf9291b53de27713f9a7c52802c33721f
Mingyee <87744503+MingyeeOAO@users.noreply.github.com>
2024-02-22 14:07:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,14 @@
 def on_forever():
     basic.show_number(200)
 basic.forever(on_forever)
+
+class Vector2:
+    def __init__(x= 0 :float, y=0 : float):
+        self.x = x;
+        self.y = y;
+
+    def __add__(self, b):
+        return Vector2(self.x+b.x, self.y+b.y)
+
+    def __len__(self):
+        return sqrt(self.x**2+self.y**2)
