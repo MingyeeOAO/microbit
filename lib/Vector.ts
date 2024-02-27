29ce6346955c96cf1bd4827715f48ad80a8e0f4c
Mingyee <87744503+MingyeeOAO@users.noreply.github.com>
2024-02-27 14:45:22 +0800

    Update Vector.ts

--- lib/Vector.ts
+++ lib/Vector.ts
@@ -2,7 +2,6 @@
 class Vector2 {
     x: number
     y: number
-    length : number
     constructor(x: number = 0, y: number = 0) {
         this.x = x
         this.y = y
@@ -13,7 +12,7 @@ class Vector2 {
         this.length = this.__len__()
     }
 
-    private __len__(): number {
+    get length(): number {
         return Math.sqrt(this.x ** 2 + this.y ** 2)
     }
     
