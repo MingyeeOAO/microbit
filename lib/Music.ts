297d515dbcadd0854f8eacf733f1c3c07df9a1b0
Mingyee <87744503+MingyeeOAO@users.noreply.github.com>
2024-03-07 14:51:54 +0800

    更新 main.ts, lib/Music.ts

--- lib/Music.ts
+++ lib/Music.ts
@@ -5,7 +5,12 @@ class Music{
     }
     play(){
         for (let x=0; x<this.notes.length; x++){
-            music.ringTone(this.notes[x]);
+            if(this.notes[x] < 0) music.rest(this.notes[x] * -1);
+            if(this.notes[x] > 0) music.ringTone(this.notes[x]);
+            //music.tonePlayable(this.notes[x], music.beat(BeatFraction.Whole))
+            //music.rest(music.beat(BeatFraction.Whole))
+            //setTimeout(() => {}, 100);
+            basic.pause(music.beat(BeatFraction.Quarter));
         }
     }
 }
--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ let color = new Color(0, 0, 0)
 //P2 is connected to LED
 radio.setGroup(87)
 basic.forever(() =>{
+    /*
     let strip = neopixel.create(DigitalPin.P2, 1, NeoPixelMode.RGB)
     
     if(input.buttonIsPressed(Button.A)){
@@ -23,8 +24,18 @@ basic.forever(() =>{
 
     if(input.buttonIsPressed(Button.B)){
         pins.digitalWritePin(DigitalPin.P1, 0)
+        
     }
     //else pins.digitalWritePin(DigitalPin.P1, 0)
     strip.showColor(color.hex)
-
+    */
+    
+    music.setTempo(114)
+    let notes= [Note.D, Note.E, Note.G, Note.E, Note.B, 0, -1, Note.B, 0, -1, Note.A, 0, 0, -1, -1*BeatFraction.Whole*4,
+    Note.D, Note.E, Note.G, Note.E, Note.A, 0, -1, Note.A, 0, -1, Note.G, 0, Note.F, Note.E, 0,
+    Note.D, Note.E, Note.G, Note.E, Note.G, 0, 0, Note.A, 0, Note.F, 0, Note.E, Note.D4, 0, 0, -1, Note.D4, 0, 0, Note.A, 0, 0, Note.G, -1, -1*BeatFraction.Whole,
+    ]
+    let melody = new Music(notes)
+    melody.play()
+    basic.pause(2000)
 })
