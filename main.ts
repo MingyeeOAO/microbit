274c0e2ccbdac6908f8690dee33e8f409a3f5355
Mingyee <87744503+MingyeeOAO@users.noreply.github.com>
2024-03-14 14:13:43 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -30,19 +30,24 @@ basic.forever(() =>{
     strip.showColor(color.hex)
     */
     
-    music.setTempo(114)
+    //music.setTempo(90)
     let notes= [Note.D, Note.E, Note.G, Note.E, Note.B, 0, -1, Note.B, 0, -1, Note.A, 0, 0, -1, -1*BeatFraction.Whole*4,
     Note.D, Note.E, Note.G, Note.E, Note.A, 0, -1, Note.A, 0, -1, Note.G, 0, Note.F, Note.E, 0,
-    Note.D, Note.E, Note.G, Note.E, Note.G, 0, 0, Note.A, 0, Note.F, 0, Note.E, Note.D4, 0, 0, -1, Note.D4, 0, 0, Note.A, 0, 0, Note.G, 0, -1, -1*BeatFraction.Whole,
+    Note.D, Note.E, Note.G, Note.E, Note.G, 0, 0, Note.A, 0, Note.F, 0, Note.E, Note.D4, 0, -1, Note.D4, 0, 0, Note.A, 0, 0, Note.G, 0, -1, -1*BeatFraction.Whole
     ]
     let notes2 = [Note.F, 0, Note.G, Note.FSharp, Note.E, Note.D, Note.C, Note.B3, Note.D, Note.B3, Note.C, Note.A3, Note.B3, Note.G3, Note.A3,
     Note.E, -1, Note.E, Note.F, -1, Note.G, -1, Note.F, -1, Note.G, -1, Note.F, Note.E, -1, 0, 0,
-     Note.E, -1, Note.E, Note.F, -1, Note.G, -1, Note.F, Note.E]
-1, Note.G, -1, Note.F, -1, Note.G, -1, Note.F, Note.E, -1
-    Note.E, -1, Note.E, Note.F, -1, Note.G, -1, Note.F, Note.E]
-    let nvg_melody = new Music(notes, 114)
-    let tbc = new Music(notes2, 90)
+     Note.E, -1, Note.E, Note.F, -1, Note.G, -1, Note.F, Note.E, -1, 0, 0, 0, 0,
+    Note.E, -1, Note.E, Note.F, -1, Note.G, -1, Note.F, -1, Note.G, -1, Note.F, Note.E, -1, 0, 0,
+    Note.E, -1, Note.E, Note.F, -1, Note.G, -1, Note.F, Note.E, -1]
+    let nvg = new Music(notes, 114)
+    let tbc = new Music(notes2, 80)
     //melody.play()
     basic.pause(2000)
-    tbc.play()
+    input.onButtonPressed(Button.A, () => {
+        tbc.play();
+    })
+    input.onButtonPressed(Button.B, () => {
+        nvg.play()
+    })
 })
