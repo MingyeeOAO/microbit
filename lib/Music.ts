0f17ede4dce1049910c48a88465e4bcd238ae972
Mingyee <87744503+MingyeeOAO@users.noreply.github.com>
2024-03-14 13:59:13 +0800

    更新 main.ts, lib/Music.ts

--- lib/Music.ts
+++ lib/Music.ts
@@ -1,9 +1,12 @@
 class Music{
     notes : number[]
-    constructor(notes : number[]){
+    bpm : number
+    constructor(notes : number[], bpm : number){
         this.notes = notes;
+        this.bpm = bpm;
     }
     play(){
+        music.setTempo(this.bpm);
         for (let x=0; x<this.notes.length; x++){
             if(this.notes[x] < 0) music.rest(this.notes[x] * -1);
             if(this.notes[x] > 0) music.ringTone(this.notes[x]);
