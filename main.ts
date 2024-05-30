fc92d8e82f59d01f4cec602f2b765ad7d86bcba7
Mingyee <87744503+MingyeeOAO@users.noreply.github.com>
2024-05-30 14:02:31 +0800

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-let color = new Color(0, 0, 0)
+
 //P1 is connected to shaker
 //P2 is connected to LED
 radio.setGroup(87)
@@ -14,13 +14,11 @@ let notes2 = [Note.F, 0, Note.G, Note.FSharp, Note.E, Note.D, Note.C, Note.B3, N
      Note.E, -1, Note.E, Note.F, -1, Note.G, -1, Note.F, Note.E, -1, 0, 0, 0, 0,
     Note.E, -1, Note.E, Note.F, -1, Note.G, -1, Note.F, -1, Note.G, -1, Note.F, Note.E, -1, 0, 0,
     Note.E, -1, Note.E, Note.F, -1, Note.G, -1, Note.F, Note.E, -1]
-function randcolor(){
-    color.r = randint(25, 50);
-    color.g = randint(25, 225);
-    color.b = randint(25, 225);
-}
+
 let cnt = 0;
-randcolor()
+let nvg = new Music(notes, 114);
+let tbc = new Music(notes2, 80)
+
 basic.forever(() =>{
     
         
