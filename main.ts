a0fe4c7a4efc5b6d99bee0516c5f3247d999c8e4
Mingyee <87744503+MingyeeOAO@users.noreply.github.com>
2024-06-06 14:04:29 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 
 //P1 is connected to shaker
 //P2 is connected to LED
-radio.setGroup(87)
-let notes= [Note.D, Note.E, Note.G, Note.E, Note.B, 0, -1, Note.B, 0, -1, Note.A, 0, 0, -1, -1*BeatFraction.Whole*4,
+/*let notes= [Note.D, Note.E, Note.G, Note.E, Note.B, 0, -1, Note.B, 0, -1, Note.A, 0, 0, -1, -1*BeatFraction.Whole*4,
     Note.D, Note.E, Note.G, Note.E, Note.A, 0, -1, Note.A, 0, -1, Note.G, 0, Note.F, Note.E, 0,
     Note.D, Note.E, Note.G, Note.E, Note.G, 0, Note.A, 0, 0, Note.F, 0, Note.E, Note.D4, 0, -1, Note.D4, 0, Note.A, 0, 0, Note.G, 0, -1, -400, 
     Note.D, Note.E, Note.G, Note.E, Note.B, 0, -1, Note.B, 0, -1, Note.A, 0, 0, -1, -1*BeatFraction.Whole*4,
@@ -14,14 +13,40 @@ let notes2 = [Note.F, 0, Note.G, Note.FSharp, Note.E, Note.D, Note.C, Note.B3, N
      Note.E, -1, Note.E, Note.F, -1, Note.G, -1, Note.F, Note.E, -1, 0, 0, 0, 0,
     Note.E, -1, Note.E, Note.F, -1, Note.G, -1, Note.F, -1, Note.G, -1, Note.F, Note.E, -1, 0, 0,
     Note.E, -1, Note.E, Note.F, -1, Note.G, -1, Note.F, Note.E, -1]
+    */
+const GROUP = 87;
+radio.setGroup(GROUP);
 const zv = new Vector2(0, 0);
+
 let cnt = 0;
-let nvg = new Music(notes, 114);
-let tbc = new Music(notes2, 80);
+//let nvg = new Music(notes, 114);
+//let tbc = new Music(notes2, 80);
 let mc = new Object(new Vector2(2, 4), 1);
 let objlist = [new Object(new Vector2(0, -1), 1)]
 //objlist[0].applyForce(new Vector2(0.5, 0.25))
+let run = true;
+let playercnt = 0;
+function shake(duration:number) {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+}
+
+let tmp = new Object(new Vector2(0, 0), 1)
+
+radio.onReceivedValue((name: string, value: number) => {
+    if(name == 'px') { tmp.position.x = 4-value}
+    if(name == 'py') { tmp.position.y = -1 - value}
+    if(name == 'vx') { tmp.velocity.x = -1*value}
+    if(name == 'vy') { tmp.velocity.y = -1*value}
+})
+radio.onReceivedString(function(val: string) {
+    if(val == 'create'){
+        objlist.push(tmp);
+    }
+})
+
 basic.forever(() =>{
+    //pins.digitalWritePin(DigitalPin.P1, 0)
+    if (run) {
     led.stopAnimation();
     mc.run();
     mc.velocity.x =0;
@@ -38,9 +63,14 @@ basic.forever(() =>{
     for(let x=0; x<objlist.length; x++){
         objlist[x].run();
         led.plot(objlist[x].position.x, objlist[x].position.y);
-        if(Math.abs(objlist[x].position.x) > 10 && Math.abs(objlist[x].position.y) > 10){
+        if(Math.abs(objlist[x].position.x) > 20 || Math.abs(objlist[x].position.y) > 20){
             objlist.splice(x, 1);
+            x--;
         }
+        if(Math.floor(objlist[x].position.x) == Math.floor(mc.position.x) && Math.floor(objlist[x].position.y) == Math.floor(mc.position.y)) {
+            shake(10);
+        }
+    }
     }
 })
 
@@ -50,7 +80,13 @@ input.onButtonPressed(Button.A, () =>{
     obj.applyForce(new Vector2(mc.velocity.x, mc.velocity.y -0.5));
 
     objlist.push(obj);
-})
 
+
+    radio.sendValue("px", obj.position.x);
+    radio.sendValue("py", obj.position.y);
+    radio.sendValue("vx", obj.velocity.x);
+    radio.sendValue("vy", obj.velocity.y);
+    radio.sendString('create')
+})
 //OLED.init(128, 64)
 //OLED.writeString("hello")
